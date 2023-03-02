c3174745a3a559ca9e21da9340222cb8b03eea2d
Amantha96 <amanthabandara1996@gmail.com>
2023-03-02 14:01:08 +0530

    first v.01

---
+++ lib/Pages/food/recommendes_food_details.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RecommendedFoodDetail extends StatelessWidget {
+  const RecommendedFoodDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            flexibleSpace: FlexibleSpaceBar(
+
+
+            ),
+
+          )
+        ],
+      ),
+    );
+  }
+}
