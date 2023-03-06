e4acc0efe15d54d7b62ffd1e8904bd0acb7c4eef
Amantha96 <amanthabandara1996@gmail.com>
2023-03-07 02:58:43 +0530

    first v.01

--- lib/helper/dependancies.dart
+++ lib/helper/dependancies.dart
@@ -1,4 +1,10 @@
+import 'package:foodapp/data/api/api_client.dart';
+import 'package:foodapp/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+
 Future<void>init()async {
+  Get.lazyPut(() => ApiClient(appBaseUrl: "http://www.dbestech.com"));
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
 
 
 }
