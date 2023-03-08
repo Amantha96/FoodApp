007fab681988aae64e988086a1953e3fbf81ac2b
Amantha96 <amanthabandara1996@gmail.com>
2023-03-08 20:30:11 +0530

    first v.01

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,6 +1,8 @@
 import 'package:foodapp/data/repository/popular_product_repo.dart';
 import 'package:get/get.dart';
 
+import '../models/products_model.dart';
+
 class PopularProductController extends GetxController{
   final PopularProductRepo popularProductRepo;
   PopularProductController({required this.popularProductRepo});
@@ -12,7 +14,7 @@ class PopularProductController extends GetxController{
     Response response = await popularProductRepo.getPopularProductList();
     if(response.statusCode==200){
       _popularProductList=[];
-      _popularProductList.addAll();
+      _popularProductList.addAll(Product.fromJson(response.body).products);
       update();
 
       
