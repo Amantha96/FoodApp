02234490e73e4649f0d27d82473236bd6b865b8d
Amantha96 <amanthabandara1996@gmail.com>
2023-03-24 14:06:27 +0530

    first v.01

--- lib/Pages/cart/cart_history.dart
+++ lib/Pages/cart/cart_history.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:foodapp/controllers/cart_controller.dart';
+import 'package:foodapp/models/cart_model.dart';
+import 'package:foodapp/utils/app_constants.dart';
 import 'package:foodapp/utils/colors.dart';
-import 'package:foodapp/widgets/Big_Text.dart';
+import 'package:foodapp/utils/dimensions.dart';
 import 'package:foodapp/widgets/app_icon.dart';
+import 'package:foodapp/widgets/big_text.dart';
+import 'package:foodapp/widgets/small_text.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class CartHistory extends StatelessWidget {
   const CartHistory({Key? key}) : super(key: key);
+
+  get i => null;
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +22,15 @@ class CartHistory extends StatelessWidget {
     Map<String, int> cartItemsPerOrders = Map();
     for (int i = 0; i < getCartHistoryList.length; i++) {
       if (cartItemsPerOrders.containsKey(getCartHistoryList[i].time!)) {
-        cartItemsPerOrders.update(getCartHistoryList[i].time!,
-                (value) => ++value);
-      } else
+        cartItemsPerOrders.update(
+            getCartHistoryList[i].time!, (value) => ++value);
+      } else {
         cartItemsPerOrders.putIfAbsent(
             getCartHistoryList[i].time!, () => 1);
+      }
     }
-    List<int> cartOrderTimeToList() {
-      return cartItemsPerOrders.entries.map((e) => e.value).toList();
-    }
-    List<int> orderTimes = cartOrderTimeToList();
-    var saveCounter = 0;
+    List<int> itemsPerOrder = cartItemsPerOrders.values.toList();
+
     return Scaffold(
       body: Column(
         children: [
@@ -48,7 +53,105 @@ class CartHistory extends StatelessWidget {
                 )
               ],
             ),
-          )
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(
+                top: Dimensions.height20,
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+              ),
+              child: MediaQuery.removePadding(
+                removeTop: true,
+                context: context,
+                child: ListView.builder(
+                  itemCount: cartItemsPerOrders.length,
+                  itemBuilder: (context, index) {
+                    var orderDate =
+                    cartItemsPerOrders.keys.elementAt(index);
+                    var itemCount = itemsPerOrder[index];
+                    int listCounter = 0;
+                     Container(
+                       height: 120,
+                      margin: EdgeInsets.only(bottom: Dimensions.height20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ((){
+                           DateTime parseDate= DateFormat("yyyy-MM-dd HH:mm:ss").parse(getCartHistoryList[listCounter].time!);
+                           var inputDate=DateTime.parse(parseDate.toString());
+                           var outputFormat= DateFormat("MM/dd/yyyy hh:mm a");
+                           var outputDate= outputFormat.format(inputDate);
+                            return BigText(text:outputDate);
+                          }()),
+                          SizedBox(height: Dimensions.height10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children:[
+                            Wrap(
+                            direction: Axis.horizontal,
+                            children: List.generate(itemCount, (index) {
+                              if (listCounter < getCartHistoryList.length) {
+                                var cartModel =
+                                getCartHistoryList[listCounter];
+                                listCounter++;
+                                return index <= 2
+                                    ? Container(
+                                  height: 80,
+                                  width: 80,
+                                  margin: EdgeInsets.only(
+                                      right: Dimensions.width10 / 2),
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                    BorderRadius.circular(
+                                        Dimensions.radius15 / 2),
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(
+                                          AppConstants.BASE_URL +
+                                              AppConstants.UPLOAD_URL +
+                                              cartModel.img!),
+                                    ),
+                                  ),
+                                )
+                                    : Container();
+                              } else {
+                                return Container();
+                              }
+                            }),
+                          ),
+                            Container(
+
+                              height: 80,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                    SmallText(text: "Total",color: AppColors.textColor),
+                                    BigText(text: itemsPerOrder[i].toString()+"Items",color: AppColors.textColor,),
+                                  Container(
+                                    padding: EdgeInsets.symmetric(horizontal: Dimensions.width10,
+                                    vertical:Dimensions.height10/2),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(Dimensions.radius15/3),
+                                      border: Border.all(width: 1,color: AppColors.mainColor),
+                                    ),
+                                    child: SmallText(text: "one more",color: AppColors.mainColor,),
+                                  )
+                                  ],
+
+                              ),
+
+                            ),
+
+                        ],
+                      )
+                        ],
+                      ),
+                    );
+                  },
+                )),
+            ))
         ],
       ),
     );
